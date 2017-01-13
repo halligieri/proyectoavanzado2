de3932747df954b0c0b0a5c8d45730c77018a71e
Luis Hereñu <halligieri@hotmail.com>
2017-01-13 02:42:16 -0300

    1
    1

---
+++ proyectoavanzado2/core/core.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ESTE ES EL NUCLEO DE LA APLICACION
+*/
+define('HTML_DIR','html/');
+define('APP_TITLE','OcrendBB');
+define('APP_COPY','Copyrigth &Copy '.date('Y',time()).' Ocrend Software.');
+require('vendor/autoload.php');
+
+?>
